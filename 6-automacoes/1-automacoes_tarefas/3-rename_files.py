c08a11a608acf06b5c99906a005fd392b0ba3448
Flavio Alves <alveslimneto@gmail.com>
2026-06-28 17:33:44 -0400

    rename_files

---
+++ 6-automacoes/1-automacoes_tarefas/3-rename_files.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+root_dir = Path('files')
+# file_paths = root_dir.iterdir()
+# for path in file_paths:
+#         print(path)
+#         for filepath in path.iterdir():
+#             print(filepath)
+file_paths = root_dir.glob('**/*') # ** ta referindo aos diretorios que venha ter 
+# e /* para fazer a leitura de todos os arquivos dentro desses diretorios
+for path in file_paths:
+    #print(path)
+    if path.is_file():
+        #print(path)
+        parent_folder = path.parts[-2]
+        new_filename = f"{parent_folder}-{path.name}"
+        #print(new_filename)
+        new_filepath = path.with_name(new_filename)
+        #print(new_filepath)
+        path.rename(new_filepath)
+        
