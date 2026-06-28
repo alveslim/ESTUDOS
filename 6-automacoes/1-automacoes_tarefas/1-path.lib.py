eb9aff52d04ed857abd86a79aadf271fa046c8fa
Flavio Alves <alveslimneto@gmail.com>
2026-06-27 20:57:10 -0400

    prefixfiles em automacoes

--- 6-automacoes/1-automacoes_tarefas/1-path.lib.py
+++ 6-automacoes/1-automacoes_tarefas/1-path.lib.py
@@ -1,17 +1,14 @@
-# modulo built-in
 from pathlib import Path
-
-p1 = Path('dados', 'texto.txt')
+p1 = Path('dados','texto.txt')
 print(p1)
 print(type(p1))
-print(p1.parent)
+print(p1.name)
 print(p1.stem)
 print(p1.suffix)
-print(p1.exists()) # false? so se estiver aberto desde um diretório diferente do newrota, se tiver aberto desde o newrota, ai sim é true   
-
+print(p1.exists())
 if p1.exists():
     with open(p1, 'r', encoding='utf-8') as file:
-        print(file.read()) # nao ta lendo se tiver aberto desde newrota
+        print(file.read())
         
 p2 = Path('dados')
 print(list(p2.iterdir()))
