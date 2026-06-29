5d42a07dd57b86b703ca822cfd786de167e54b56
Flavio Alves <alveslimneto@gmail.com>
2026-06-29 19:02:01 -0400

    5-datafiles

--- 6-automacoes/1-automacoes_tarefas/5-data_files.py
+++ 6-automacoes/1-automacoes_tarefas/5-data_files.py
@@ -6,11 +6,6 @@ root_dir = Path('files')
 for path in root_dir.glob('**/*'):
     if path.is_file():
         
-        # nome:
-        parent_folder = path.parts[-2]
-        new_filename = f"{parent_folder}-{path.name}"
-        #print(new_filename)
-        new_filepath = path.with_name(new_filename)
         
         # data & hora
         stats = path.stat()
@@ -20,6 +15,8 @@ for path in root_dir.glob('**/*'):
         date_modified = date_created.strftime('%Y-%m-%d_%H:%M:%S')
         #print(f"Date modified: {date_modified}")
         
-        final_filename = f"{new_filepath}-{date_modified}"
-        final_filepath = path.with_name(final_filename)
-        print(final_filepath)
+        # nome:
+        new_filename = f"{date_modified}-{path.name}"
+        #print(new_filename)
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
