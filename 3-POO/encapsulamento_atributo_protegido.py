33a60e3304af77c9fce5351b5d333c2358d3c0bd
Flavio Alves <alveslimneto@gmail.com>
2026-07-20 19:41:37 -0400

    add: encapsulamento de atributo

---
+++ 3-POO/encapsulamento_atributo_protegido.py
@@ -0,0 +1,30 @@
+"""4. Encapsulamento com Atributo Protegido (_) vs Privado (__)
+
+No Python, por convenção da comunidade, costuma-se usar apenas um underline (_valor) para indicar que algo 
+é privado/interno, em vez de dois (__valor), evitand do o Name Mangling (mangling de nomes).
+
+Desafio: Reescreva a sua classe original Minhaclasse alterando o atributo de self.__valor para self._valor.
+
+    Implemente a @property e o @valor.setter.
+
+    Adicione um print dentro do getter e do setter indicando quando cada um está sendo chamado."""
+
+class Minhaclasse:
+    def __init__(self)-> None:
+        self._valor = None
+
+    @property
+    def valor(self) -> type:
+        print("calling getter...")
+        return self._valor
+    
+    @valor.setter
+    def valor(self, valor: type) -> type:
+        print("calling setter")
+        self._valor = valor
+
+"""-----------------------------------------------------------------------"""
+
+my_class = Minhaclasse()
+my_class.valor = 10
+print(my_class.valor)
