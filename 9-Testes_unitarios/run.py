17d65e7026333a77a38dab5869943a4932d7a989
Flavio Alves <alveslimneto@gmail.com>
2026-08-16 19:56:11 -0400

    testes unitarios

---
+++ 9-Testes_unitarios/run.py
@@ -0,0 +1,12 @@
+def divisao(x: int) -> float:
+    return x / 2
+
+def informacoes() -> dict:
+    # imagine que e uma funcao muito complexa
+    # processamento
+    # e retorna tal informacao:
+    return {
+        "is_ok": True,
+        "name": "Rafa Lhama",
+        "age": 28
+    }
---
+++ 9-Testes_unitarios/test/run_test.py
@@ -0,0 +1,17 @@
+from run import divisao, informacoes
+
+def test_divisao():
+    val = 8
+    resp = divisao(val)
+    print(resp)
+    
+    assert resp == val / 2 # assert e uma validacao em python
+    assert isinstance(resp, float) # sentencas
+    
+def test_informacoes():
+    resp = informacoes()
+    assert isinstance(resp, dict)
+    assert "name" in resp
+    assert "Height" not in resp # sempre colocando instrucoes
+    assert "Rafa" in resp["name"]
+    
