3bf545ec7f05d59ed7c0b71a8dbd53e72915603a
Flavio Alves <alveslimneto@gmail.com>
2026-08-27 15:26:03 -0400

    tecnica de do while em python

---
+++ 1-python/30-do_while.py
@@ -0,0 +1,7 @@
+while True:
+    # O código aqui dentro SEMPRE roda pelo menos uma vez
+    resposta = input("Digite 'sair' para encerrar: ")
+    
+    # Condição de verificação (o "while" do final)
+    if resposta == 'sair':
+        break  # Sai do loop imediatamente
