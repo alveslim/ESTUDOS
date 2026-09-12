a5f3274a571a5570e07fd3c07e093c8de2ffab88
Flavio Alves <alveslimneto@gmail.com>
2026-09-12 18:15:36 -0400

    flet: aula 7 criando funcoes na class checkbox

---
+++ 10-Flet/7-aula/main.py
@@ -0,0 +1,34 @@
+import flet as ft
+from custom_checkbox import Checkbox
+
+def main(page: ft.Page):
+    page.title = 'flet app'
+    page.padding = 20
+    
+    def add_task(e):
+        task_list.controls.append(Checkbox(new_task.value))
+        new_task.value = ''
+        page.update()
+        new_task.focus()
+        
+    new_task = ft.TextField(hint_text="Insira uma tarefa", expand=True,
+                            autofocus=True)
+    new_button = ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_task)
+    
+    task_list = ft.Column()
+    task_column = ft.Column(
+        width=400,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    new_button
+                ]
+            ),
+            task_list,
+        ]
+    )
+    
+    page.add(task_column)
+    
+ft.app(target=main)
